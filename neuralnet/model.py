e429d5cb8bd021d087b8a49c386c7ac7fcb5e6a1
synguyen446 <nsn131203@gmail.com>
2026-07-10 16:13:51 -0700

    added bias and def fit

--- neuralnet/model.py
+++ neuralnet/model.py
@@ -8,6 +8,7 @@ class Model:
         self.loss_fn = None
         self.y_true = None
         self.gradients = []
+        self.bias_gradient = []
 
     def forward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
         self.y_true = y
@@ -21,22 +22,47 @@ class Model:
 
     def backward(self):
         self.gradients = []
+        self.bias_gradient = []
         dout = 1
         for i in range(len(self.dag) - 1, -1, -1):
-            dw, dout = self.dag[i].backward(dout, self.y_true)
+            dw, db, dout = self.dag[i].backward(dout, self.y_true)
             self.gradients.append(dw)
-        return self.gradients
+            self.bias_gradient.append(db)
+        return self.gradients, self.bias_gradient
 
     def step(self, alpha):
         self.gradients.reverse()
+        self.bias_gradient.reverse()
         for i in range(len(self.dag) - 1, -1, -1):
             self.dag[i].weight -= alpha * self.gradients[i]
+            self.dag[i].bias -= alpha * self.bias_gradient[i]
 
     def get_weights(self):
         weights = []
         for i in range(len(self.dag)):
             weights.append(self.dag[i].weight)
         return weights
+
+    def fit(self, epochs, batch_size, X_train, y_train):
+        for i in range(epochs):
+            indices = np.random.permutation(len(X_train))
+            epoch_loss = 0
+            correct_count = 0
+
+            X_shuffle = X_train[indices]
+            y_shuffle = y_train[indices]
+            for start in range(0, len(X_train), batch_size):
+                X_batch = X_shuffle[start : start + batch_size]
+                y_batch = y_shuffle[start : start + batch_size]
+                y_pred_prob, loss = self.forward(X_batch, y_batch)
+                y_pred = y_pred_prob.argmax(axis=1)
+                correct_count += np.sum(y_pred == y_batch.argmax(axis=-1))
+                epoch_loss += loss * len(X_batch)
+                self.backward()
+                self.step(0.003)
+            print(
+                f"STEP {i + 1}: Loss - {epoch_loss/len(X_train):.4f} | Accuracy - {correct_count/len(X_train):.4f}"
+            )
 
     def compile(self, loss_fn):
         self.loss_fn = loss_fn
@@ -48,6 +74,7 @@ class Layer:
         self.out_feature = out_feature
         self.activation = activation
         self.weight = xavier_uniform_init(in_feature, out_feature)
+        self.bias = np.zeros((1, out_feature))
         self.a_previous = None
         self.z_current = None
         self.a_current = None
@@ -58,12 +85,13 @@ class Layer:
         y_true=None,
     ):
         delta = dout * self.activation.backward(self.a_current, y_true)
-        dw = self.a_previous.T @ delta
+        dw = (self.a_previous.T @ delta) / self.a_current.shape[0]
+        db = np.sum(delta, axis=0, keepdims=True) / self.a_current.shape[0]
         dout = delta @ self.weight.T
-        return dw, dout
+        return dw, db, dout
 
     def __call__(self, x):
         self.a_previous = x
-        self.z_current = x @ self.weight
+        self.z_current = x @ self.weight + self.bias
         self.a_current = self.activation.forward(self.z_current)
         return self.a_current
